14c883d34469207df9b92f58985fbc4513eb6d00
08012075 <08012075@sfcollege.edu>
2026-01-30 20:36:01 -0500

    Lesson 2.1 complete Player Positioning

--- Units/Basic Gameplay/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Units/Basic Gameplay/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float horizontalInput;
-    [SerializeField] private float speed = 20.0f;
-    [SerializeField] private GameObject projectilePrefab;
-    public float xRange = 20.0f;
+    // Variables for player
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,20 +16,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Keeps player in bounds
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
-        else if (transform.position.x > xRange)
+
+        if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate (projectilePrefab, transform.position, projectilePrefab.transform.rotation);
-        }
+        // Movement for player
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
