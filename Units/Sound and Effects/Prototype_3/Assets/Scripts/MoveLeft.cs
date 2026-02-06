638de2e036e63c610017241a4eb975cca6c48022
08012075 <08012075@sfcollege.edu>
2026-02-05 22:17:48 -0500

    Completed Lesson 3.1 Jump Force

--- Units/Sound and Effects/Prototype_3/Assets/Scripts/MoveLeft.cs
+++ Units/Sound and Effects/Prototype_3/Assets/Scripts/MoveLeft.cs
@@ -3,21 +3,15 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 30;
-    private PlayerController playerControllerScript;
-    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // It looks for the player object in the hierarchy and gets the PlayerController script
-        // attached to it
-        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (playerControllerScript.gameOver == false) 
-        {
-            transform.Translate(Vector3.left * Time.deltaTime * speed);
-        }
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
     }
 }
--- Units/Sound and Effects/Prototype_3/Assets/Scripts/SpawnManager.cs
+++ Units/Sound and Effects/Prototype_3/Assets/Scripts/SpawnManager.cs
@@ -2,40 +2,25 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject rocketPrefab;
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
-    private Vector3 spawnPos4rocket = new Vector3(27, 5 , 0);
     private float startDelay = 2;
     private float repeatRate = 2;
-    private float startDelayR = 3;
-    private float repeatRateR = 4;
-    private PlayerController playerControllerScript;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
-        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
-        InvokeRepeating("SpawnRocket", startDelayR, repeatRateR);
+        InvokeRepeating("SpawnObject", startDelay, repeatRate);
     }
 
     // Update is called once per frame
     void Update()
     {
-    
+        
     }
-void SpawnObstacle()
+
+    void SpawnObject()
     {
-        if (playerControllerScript.gameOver == false) 
-        {
         Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
-        }
-    }
-void SpawnRocket()
-    {
-        if (playerControllerScript.gameOver == false)
-        {
-            Instantiate(rocketPrefab, spawnPos4rocket, rocketPrefab.transform.rotation);
-        }
     }
 }
-
