b5a34eeb4e3d7e9a73d229e4343108aeb736e63b
08012075 <08012075@sfcollege.edu>
2026-02-13 16:51:48 -0500

    Completed Lesson 4.3 PowerUp and CountDown

--- Units/Gameplay Mechanics/Prototype 4/Assets/Scripts/PlayerController.cs
+++ Units/Gameplay Mechanics/Prototype 4/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,16 @@
+using System;
 using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
     // Variables
-    public Rigidbody playerRb;
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+    private float powerUpStrength = 15.0f;
     public float speed = 5.0f;
-    public GameObject focalPoint;
+    public bool hasPowerup = false;
+    public GameObject powerupIndicator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,5 +24,36 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+        }
     }
 }
